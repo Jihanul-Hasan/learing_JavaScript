8a93650b54c8f81030288b6ba82a109e68fc8ee8
Jihanul-Hasan <jihanul.hasan2000@gmail.com>
2022-07-05 01:43:34 +0600

    Button events

---
+++ index.js
@@ -0,0 +1,9 @@
+console.log("helo world");
+
+
+function clicked(){
+    let jihan=document.getElementById('mybtn');
+    jihan.innerHTML="Clicked";
+}
+
+container2.addEventListener('click',function(){ console.log("baaaaal")})
